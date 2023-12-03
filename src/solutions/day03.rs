5ef5aee611d722005c83c89addcd92864c2524d6
Bela Stoyan <bela.stoyan@gmail.com>
2023-12-03 20:40:11 +0100

    rework neighborhood lookup

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -1,4 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    cmp::{max, min},
+    collections::{HashMap, HashSet},
+};
 
 #[derive(Debug)]
 enum Symbol {
@@ -17,27 +20,39 @@ impl Symbol {
     }
 }
 
-#[derive(Debug)]
-struct ContingousNumber {
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Coord {
     x: usize,
     y: usize,
+}
+
+#[derive(Debug)]
+struct ContingousNumber {
+    coord: Coord,
     length: u8,
     value: u32,
 }
 
-struct Board {
-    symbols: HashMap<(usize, usize), char>,
-    numbers: HashMap<(usize, usize), ContingousNumber>,
+struct RawBoard {
+    digits: HashMap<Coord, Coord>,
+    numbers: HashMap<Coord, ContingousNumber>,
+    symbols: HashMap<Coord, char>,
     size: (usize, usize),
 }
 
-fn extract_board_information(input: &str) -> Board {
+struct Board {
+    numbers: HashMap<Coord, ContingousNumber>,
+    adjacent_numbers_to_symbols: HashMap<Coord, (char, HashSet<Coord>)>,
+}
+
+fn parse_input(input: &str) -> RawBoard {
     let mut symbols = HashMap::new();
     let mut numbers = HashMap::new();
+    let mut digits = HashMap::new();
 
     let mut store_number = |current_number: &mut Option<ContingousNumber>| {
         if let Some(num) = current_number.take() {
-            numbers.insert((num.x, num.y), num);
+            numbers.insert(num.coord, num);
         }
     };
 
@@ -64,21 +79,21 @@ fn extract_board_information(input: &str) -> Board {
             match s {
                 Symbol::Digit(d) => {
                     let num = current_number.get_or_insert(ContingousNumber {
-                        x,
-                        y,
+                        coord: Coord { x, y },
                         length: 0,
                         value: 0,
                     });
 
                     num.length += 1;
                     num.value = num.value * 10 + d as u32;
+                    digits.insert(Coord { x, y }, num.coord);
                 }
                 _ => {
                     // end current number parsing, if any
                     store_number(&mut current_number);
 
                     if let Symbol::Other(c) = s {
-                        symbols.insert((x, y), c);
+                        symbols.insert(Coord { x, y }, c);
                     }
                 }
             }
@@ -88,87 +103,99 @@ fn extract_board_information(input: &str) -> Board {
         store_number(&mut current_number);
     }
 
-    Board {
-        symbols,
+    RawBoard {
+        digits,
         numbers,
+        symbols,
         size: (size.0.unwrap_or(0), size.1),
     }
 }
 
-/// Iterate over all number digits and call the action function for each digit.
-/// if action returns true, the iteration will be stopped.
-fn iterate_number_digits<F>(
-    numbers: &HashMap<(usize, usize), ContingousNumber>,
-    width: usize,
-    height: usize,
-    mut action: F,
-) where
-    F: FnMut(&ContingousNumber, usize, usize) -> bool,
-{
-    for number in numbers.values() {
-        for x in number.x..number.x + number.length as usize {
-            for sx in x.saturating_sub(1)..=std::cmp::min(x + 1, width - 1) {
-                for sy in number.y.saturating_sub(1)..=std::cmp::min(number.y + 1, height - 1) {
-                    if sx == x && sy == number.y {
-                        continue;
-                    }
-                    if action(number, sx, sy) {
-                        break;
-                    }
-                }
+fn process_board(input: &str) -> Board {
+    let RawBoard {
+        digits,
+        numbers,
+        symbols,
+        size,
+    } = parse_input(input);
+
+    // build up adjacency map for symbols
+    let mut adjacent_numbers_to_symbols = HashMap::new();
+
+    for (symbol_cord, symbol) in symbols.into_iter() {
+        let mut adjacent_numbers = HashSet::new();
+
+        let x_range =
+            max(symbol_cord.x as isize - 1, 0) as usize..=min(symbol_cord.x + 1, size.0 - 1);
+
+        let y_range =
+            max(symbol_cord.y as isize - 1, 0) as usize..=min(symbol_cord.y + 1, size.1 - 1);
+
+        for (x, y) in x_range.flat_map(|x| y_range.clone().map(move |y| (x, y))) {
+            let lookup_coord = Coord { x, y };
+
+            if lookup_coord == symbol_cord {
+                continue;
+            }
+
+            if let Some(number_cord) = digits.get(&lookup_coord) {
+                adjacent_numbers.insert(*number_cord);
             }
         }
+
+        if !adjacent_numbers.is_empty() {
+            adjacent_numbers_to_symbols.insert(symbol_cord, (symbol, adjacent_numbers));
+        }
+    }
+
+    Board {
+        adjacent_numbers_to_symbols,
+        numbers,
     }
 }
 
 pub fn part_one(input: &str) -> u32 {
     let Board {
-        symbols,
         numbers,
-        size: (width, height),
-    } = extract_board_information(input);
-    let mut part_numbers = HashMap::new();
+        adjacent_numbers_to_symbols,
+    } = process_board(input);
 
-    iterate_number_digits(&numbers, width, height, |number, sx, sy| {
-        if symbols.get(&(sx, sy)).is_some() {
-            part_numbers.insert((number.x, number.y), number.value);
-            true // break the loop
-        } else {
-            false
+    let mut part_numbers = HashSet::new();
+
+    for (_, numbers) in adjacent_numbers_to_symbols.values() {
+        for start_cord in numbers {
+            part_numbers.insert(start_cord);
         }
-    });
+    }
 
-    part_numbers.values().sum()
+    part_numbers
+        .iter()
+        .map(|c| numbers.get(c).unwrap().value)
+        .sum()
 }
 
 pub fn part_two(input: &str) -> u32 {
     let Board {
-        symbols,
         numbers,
-        size: (width, height),
-    } = extract_board_information(input);
-    let mut adjacent_numbers_of_gear = HashMap::new();
+        adjacent_numbers_to_symbols,
+    } = process_board(input);
 
-    iterate_number_digits(&numbers, width, height, |number, sx, sy| {
-        if let Some('*') = symbols.get(&(sx, sy)) {
-            adjacent_numbers_of_gear
-                .entry((sx, sy))
-                .or_insert_with(HashSet::new)
-                .insert((number.x, number.y));
+    let mut total_gear_ratios = 0;
+
+    for (c, adjacent_numbers) in adjacent_numbers_to_symbols.values() {
+        if *c != '*' || adjacent_numbers.len() != 2 {
+            continue;
         }
-        false // keep going
-    });
 
-    adjacent_numbers_of_gear
-        .iter()
-        .filter(|(_, cords)| cords.len() == 2)
-        .map(|(_, cords)| {
-            cords
-                .iter()
-                .map(|(x, y)| numbers.get(&(*x, *y)).unwrap().value)
-                .product::<u32>()
-        })
-        .sum()
+        let gear_ratio = adjacent_numbers
+            .iter()
+            .map(|c| numbers.get(c).unwrap().value)
+            .product::<u32>();
+
+        total_gear_ratios += gear_ratio;
+    }
+
+    total_gear_ratios
 }
 
 #[cfg(test)]
